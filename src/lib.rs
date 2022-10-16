1e918d533fd44208b4843728d18213ef65f179fb
Isitha Subasinghe <isubasinghe@student.unimelb.edu.au>
2022-10-16 13:05:40 +1100

    added stuff

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,9 @@ use std::{
     sync::atomic::{AtomicBool, AtomicUsize, Ordering},
 };
 
+const CONTENTION_THRESHOLD: usize = 2;
+const RETRY_THRESHOLD: usize = 2;
+
 pub trait ContentionMeasure {
     fn new() -> Self;
     fn detected(&self) -> bool;
@@ -20,7 +23,7 @@ impl ContentionMeasure for CounterContentionMeasure {
         CounterContentionMeasure { countention_cnt: 0 }
     }
     fn detected(&self) -> bool {
-        self.countention_cnt >= 10
+        self.countention_cnt > CONTENTION_THRESHOLD
     }
     fn detect(&mut self) {
         self.countention_cnt += 1;
@@ -52,10 +55,10 @@ pub trait NormalisedLockFree {
         &self,
         result: Result<(), usize>,
         performed: &Self::Cases,
-        contention: &mut Self::ContentionMeasure
+        contention: &mut Self::ContentionMeasure,
     ) -> Result<Self::Output, ()>;
 }
-pub struct Help {
+pub struct OperationRecord {
     completed: AtomicBool,
     at: AtomicUsize,
 }
@@ -63,11 +66,11 @@ pub struct Help {
 struct HelpQueue {}
 
 impl HelpQueue {
-    pub fn add(&self, help: *const Help) {}
-    pub fn peek(&self) -> Option<*const Help> {
+    pub fn add(&self, help: *const OperationRecord) {}
+    pub fn peek(&self) -> Option<*const OperationRecord> {
         todo!()
     }
-    pub fn try_remove_front(&self, completed: *const Help) {
+    pub fn try_remove_front(&self, completed: *const OperationRecord) {
         todo!()
     }
 }
@@ -96,27 +99,36 @@ impl<LF: NormalisedLockFree> WaitFreeSimulator<LF> {
         if let Some(help) = self.help_queue.peek() {}
     }
     pub fn run(&self, op: LF::Input) -> LF::Output {
-        let mut fast = true;
-        loop {
-            if fast {
-                let help = /* once in a while */ false;
-                if help {
-                    self.help()
-                }
-            } else {
+        // fast path
+        for retry in 0.. {
+            let help = /* once in a while */ false;
+            if help {
+                self.help();
+            }
+            let mut contention = LF::ContentionMeasure::new();
+            if contention.detected() {
+                break;
+            }
+            let cas = self.lf.generator(&op, &mut contention);
+            if contention.detected() {
+                break;
+            }
+            let result = self.cas_execute(&cas, &mut contention);
+            if contention.detected() {
+                break;
+            }
+            match self.lf.wrap_up(result, &cas, &mut contention) {
+                Ok(outcome) => return outcome,
+                Err(()) => {}
+            };
+            if contention.detected() {}
 
+            if retry > RETRY_THRESHOLD {
+                // slow  path
+                break;
             }
 
-            fast = false;
-            let mut contention = LF::ContentionMeasure::new();
-            let cas = self.lf.generator(&op, &mut contention);
-            let result = self.cas_execute(&cas, &mut contention);
-            match self.lf.wrap_up(result, &cas, &mut contention) {
-                Ok(outcome) => outcome,
-                Err(()) => continue,
-            };
-
-            if let Err(cnt) = result {
+            /* if let Err(cnt) = result {
                 // slow path
                 let help = Help {
                     completed: AtomicBool::new(false),
@@ -126,19 +138,30 @@ impl<LF: NormalisedLockFree> WaitFreeSimulator<LF> {
                 while !help.completed.load(Ordering::SeqCst) {
                     self.help();
                 }
-            }
+            } */
         }
+
+        let help = OperationRecord {
+            completed: AtomicBool::new(false),
+            at: AtomicUsize::new(0),
+        };
+        self.help_queue.add(&help);
+        while !help.completed.load(Ordering::SeqCst) {
+            self.help();
+        }
+        unreachable!()
     }
 }
 
 /**
+ * ABA problem
  * head -> A (@0x1)
  * insert B (@0x2); B.next = 0x1
  * CAS(head, 0x1, 0x2)
- * succeed if A is still at the head 
+ * succeed if A is still at the head
  * fail if A is no longer at the head
  * imagine above has not yet executed
- * meanwhile 
+ * meanwhile
  * insert C (@0x3) C.next = 0x1
  * CAS(head, 0x1, 0x3)
  * remove A
@@ -146,7 +169,7 @@ impl<LF: NormalisedLockFree> WaitFreeSimulator<LF> {
  * insert D (@0x1); D.next = 0x3
  * CAS(head, 0x3, 0x1)
  * head -> D(@0x1) -> C(@0x3) -> .
- * CAS(head, 0x1, 0x2) 
+ * CAS(head, 0x1, 0x2)
  * head -> B(@0x2) -> A(0x1) actually D(@0x1) -> C(@0x3) -> .
  * It works out here due to A and D having the same address, but if we imagine a doubly linked list it doesn't
  * This CAS should have failed
